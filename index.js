9745390ef382927fde4ac844714f7ca88c009f65
swilsonwinters <swilsonwinters@gmail.com>
2022-08-26 18:11:01 -0400

    importing packages

---
+++ index.js
@@ -0,0 +1,6 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+//const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
