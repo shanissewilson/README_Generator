b7a28e1eef7d98d7ef611efaf6e8d3742a953ad1
swilsonwinters <swilsonwinters@gmail.com>
2022-08-26 18:15:54 -0400

    added generateMarkdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `\n* [License](#license)\n`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License This project is licensed under the ${license} license.`
+  }
+  return ""; 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+## Description 
+${data.description}
+## Table of Contents
+*[Installation](#installation)
+*[Usage](#usage)
+${renderLicenseLink(data.license)}
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+## Installation
+Run the following command to install:
+\`\`\`
+${data.installation}
+\`\`\`
+## Usage
+${data.usage}
+${renderLicenseSection(data.license)}
+## Contributing
+${data.contributing}
+## Tests
+Run the following command for testing: 
+\`\`\`
+${data.tests}
+\`\`\`
+## Questions 
+Any questions? Contact me at ${data.email}. Check out my work at [${data.github}](https://github.com/${data.github})
 `;
 }
-
 module.exports = generateMarkdown;
